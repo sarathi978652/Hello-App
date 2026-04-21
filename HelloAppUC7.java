b5d692b61c0233369d1ac6fff3ebeec9296abcb7
sarathi978652 <sarathi978652@gmail.com>
2026-04-21 14:02:34 +0530

    UC7

--- HelloAppUC7.java
+++ HelloAppUC7.java
@@ -1,10 +1,10 @@
 public class HelloAppUC7 {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            String result = "Hello " + String.join(" ", args);
-            System.out.println(result);
-        } else {
-            System.out.println("Hello");
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
         }
+        String names = String.join(", ", args);
+        System.out.println("Hello, " + names + "!");
     }
 }
